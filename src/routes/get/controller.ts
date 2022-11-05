a3daaa9d754bbb975a76ff86a86f080c576509ea
Abhinandan Sharma <abhi.moudgil15@gmail.com>
2022-11-06 00:45:21 +0530

    feat:added response

--- src/routes/get/controller.ts
+++ src/routes/get/controller.ts
@@ -1,3 +1,12 @@
-export const addGetCall = async (req: Request, res: Response) => {
-    console.log(req.body);
+export const addGetCall = async (req: Request, res: Response,next:any) => {
+    console.log(req);
+    const endpoint=req.url;
+    let responseFound={}
+    fetch(endpoint).then((response)=>{
+        response.json().then((data)=>{
+            responseFound=data;
+        })
+    })
+    console.log(endpoint,req.body,responseFound)
+    return responseFound;
 };
